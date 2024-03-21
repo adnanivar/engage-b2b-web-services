1ce242b3a2c1c13efc788fe39819f900afdb632e
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2024-03-21 20:43:06 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,26 @@
+    main()
+
+    for item in data:
+def main():
+    return data
+    data = generate_random_data()
+
+
+if __name__ == "__main__":
+def generate_random_data():
+import random
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+
+        }
+        int[] data = new int[10];
+        }
+    }
+}
